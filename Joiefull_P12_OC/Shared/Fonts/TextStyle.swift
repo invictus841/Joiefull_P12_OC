f440caa7d066a5e4311d25db357ee22e2d86df9b
invictus841 <usedforfunny@gmail.com>
2025-05-08 17:23:17 +0200

    Added a Textsyle enum, and a textStyle modifier to know in advance which font, size to show when isDetail, and when its on iPad.

---
+++ Joiefull_P12_OC/Shared/Fonts/TextStyle.swift
@@ -0,0 +1,59 @@
+//
+//  TextStyle.swift
+//  Joiefull_P12_OC
+//
+//  Created by Alexandre Talatinian on 08/05/2025.
+//
+
+import Foundation
+import SwiftUI
+
+enum TextStyle {
+    case sectionTitle
+    case itemName(isPad: Bool, isDetail: Bool)
+    case itemPrice(isPad: Bool, isDetail: Bool)
+    case ratingAverage(isPad: Bool, isDetail: Bool)
+    case favoriteCount(isPad: Bool, isDetail: Bool)
+    case description(isPad: Bool)
+
+    var font: Font {
+        switch self {
+        case .sectionTitle:
+            return .custom("OpenSans-SemiBold", size: 22)
+
+        case .itemName(let isPad, let isDetail):
+            if isDetail {
+                return .custom("OpenSans-SemiBold", size: isPad ? 22 : 18)
+            } else {
+                return .custom("OpenSans-SemiBold", size: isPad ? 18 : 14)
+            }
+
+        case .itemPrice(let isPad, let isDetail):
+            return .custom("OpenSans-Regular", size: isPad
+                ? (isDetail ? 22 : 18)
+                : (isDetail ? 18 : 14)
+            )
+
+        case .ratingAverage(let isPad, let isDetail):
+            return .custom("OpenSans-Regular", size: isPad
+                ? (isDetail ? 22 : 18)
+                : (isDetail ? 18 : 14)
+            )
+
+        case .favoriteCount(let isPad, let isDetail):
+            return .custom("OpenSans-SemiBold", size: isPad
+                ? (isDetail ? 22 : 14)
+                : (isDetail ? 18 : 14)
+            )
+
+        case .description(let isPad):
+            return .custom("OpenSans-Regular", size: isPad ? 18 : 14)
+        }
+    }
+}
+
+extension Text {
+    func textStyle(_ style: TextStyle) -> some View {
+        self.font(style.font)
+    }
+}
