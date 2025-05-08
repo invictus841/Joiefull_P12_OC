7b5d6a43f3789fbb94cb0b4354d1de275682ced7
invictus841 <usedforfunny@gmail.com>
2025-05-08 17:37:03 +0200

    Added a basic service api harcoded endpoint, to fetch the items

---
+++ Joiefull_P12_OC/APIService.swift
@@ -0,0 +1,23 @@
+//
+//  APIService.swift
+//  Joiefull_P12_OC
+//
+//  Created by Alexandre Talatinian on 08/05/2025.
+//
+
+import Foundation
+
+final class APIService {
+    static let shared = APIService()
+
+    private let urlString = "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/Cr-ez-une-interface-dynamique-et-accessible-avec-SwiftUI/main/api/clothes.json"
+
+    func fetchClothingItems() async throws -> [ClothingItem] {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([ClothingItem].self, from: data)
+    }
+}
