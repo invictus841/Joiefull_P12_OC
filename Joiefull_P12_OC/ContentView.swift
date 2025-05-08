92f45331186546b568899d2e6aa4e1749a54a6a9
invictus841 <usedforfunny@gmail.com>
2025-05-08 16:31:45 +0200

    Initial Commit

---
+++ Joiefull_P12_OC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Joiefull_P12_OC
+//
+//  Created by Alexandre Talatinian on 08/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Joiefull_P12_OC/Joiefull_P12_OCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Joiefull_P12_OCApp.swift
+//  Joiefull_P12_OC
+//
+//  Created by Alexandre Talatinian on 08/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Joiefull_P12_OCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
