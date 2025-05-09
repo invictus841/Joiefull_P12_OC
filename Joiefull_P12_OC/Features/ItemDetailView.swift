5202b9e793788cb977b8bf0e7d63d3b645b2825d
invictus841 <usedforfunny@gmail.com>
2025-05-09 10:24:55 +0200

    Added basic ItemDetailView, and a reusable FavoriteBubble component.

---
+++ Joiefull_P12_OC/Features/ItemDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemDetailView.swift
+//  Joiefull_P12_OC
+//
+//  Created by Alexandre Talatinian on 09/05/2025.
+//
+
+import SwiftUI
+
+struct ItemDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemDetailView()
+}
---
+++ Joiefull_P12_OC/Shared/Components.swift
@@ -0,0 +1,18 @@
+//
+//  Components.swift
+//  Joiefull_P12_OC
+//
+//  Created by Alexandre Talatinian on 09/05/2025.
+//
+
+import SwiftUI
+
+struct Components: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Components()
+}
