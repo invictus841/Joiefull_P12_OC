9e079ff63c9c5f9146669193e40b7b30fab397e3
invictus841 <usedforfunny@gmail.com>
2025-05-16 09:19:56 +0200

    Added func share item in detail view

--- Joiefull_P12_OC/Features/ItemDetailView.swift
+++ Joiefull_P12_OC/Features/ItemDetailView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ItemDetailView: View {
+    @Environment(\.horizontalSizeClass) private var sizeClass
     @Environment(\.presentationMode) var presentationMode
 
     let item: ClothingItem
-    let isPad: Bool
-    
     @State private var userComment: String = ""
+
+    var isPad: Bool {
+        sizeClass == .regular
+    }
 
     var imageSize: CGSize {
         isPad ? CGSize(width: 451, height: 408) : CGSize(width: 328, height: 431)
@@ -36,10 +39,8 @@ struct ItemDetailView: View {
         }
         .navigationBarBackButtonHidden()
     }
-}
 
-private extension ItemDetailView {
-    var imageSection: some View {
+    private var imageSection: some View {
         ZStack(alignment: .top) {
             AsyncImage(url: item.picture.url) { phase in
                 if let image = phase.image {
@@ -83,7 +84,7 @@ private extension ItemDetailView {
                 }
             }
             .padding()
-            
+
             VStack {
                 Spacer()
                 HStack {
@@ -92,36 +93,32 @@ private extension ItemDetailView {
                 }
             }
         }
-
     }
-    
-    var headerSection: some View {
+
+    private var headerSection: some View {
         HStack(alignment: .top) {
-            HStack(spacing: 8) {
-                Text(item.name)
-                    .textStyle(.itemName(isPad: isPad, isDetail: true))
-                
-                Spacer()
-                
-                HStack(spacing: 4) {
-                    Image("filledStar")
-                        .resizable()
-                        .frame(width: 14, height: 14)
-                    Text("4.5")
-                        .textStyle(.ratingAverage(isPad: isPad, isDetail: true))
-                }
+            Text(item.name)
+                .textStyle(.itemName(isPad: isPad, isDetail: true))
+
+            Spacer()
+
+            HStack(spacing: 4) {
+                Image("filledStar")
+                    .resizable()
+                    .frame(width: 14, height: 14)
+                Text("4.5")
+                    .textStyle(.ratingAverage(isPad: isPad, isDetail: true))
             }
-            
         }
     }
 
-    var priceSection: some View {
+    private var priceSection: some View {
         HStack {
             Text(String(format: "%.2f €", item.price))
                 .textStyle(.itemPrice(isPad: isPad, isDetail: true))
 
             Spacer()
-            
+
             if item.isDiscounted {
                 Text(String(format: "%.2f €", item.originalPrice))
                     .textStyle(.itemPrice(isPad: isPad, isDetail: true))
@@ -131,13 +128,13 @@ private extension ItemDetailView {
         }
     }
 
-    var descriptionSection: some View {
+    private var descriptionSection: some View {
         Text(item.picture.description)
             .textStyle(.description(isPad: isPad))
             .padding(.top, 8)
     }
 
-    var ratingSection: some View {
+    private var ratingSection: some View {
         HStack(spacing: 14) {
             Image("userPic")
                 .resizable()
@@ -155,54 +152,44 @@ private extension ItemDetailView {
         }
         .padding(.top, 16)
     }
-    
-    var commentSection: some View {
-            TextField("Partagez ici vos impressions sur cette pièce", text: $userComment)
-                .frame(height: isPad ? 69 : 53)
-                .padding(.trailing, 12)
-                .padding(.leading, 8)
-                .padding(.top, 0)
-                .padding(.bottom, 18)
-                .background(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.gray.opacity(0.3), lineWidth: 1)
-                )
-                .font(.custom("OpenSans-Regular", size: isPad ? 18 : 14))
-                .accessibilityLabel(Text("Zone de texte pour vos impressions"))
-                .padding(.vertical)
-        }
-}
 
-// MARK: - helpers
-private extension ItemDetailView {
-    func shareItem() {
-        let text = """
-        \(item.name) – \(String(format: "%.2f €", item.price))
-        Découvrez cette pièce sur l’app Joiefull!
-        """
+    private var commentSection: some View {
+        TextField("Partagez ici vos impressions sur cette pièce", text: $userComment)
+            .frame(height: isPad ? 69 : 53)
+            .padding(.trailing, 12)
+            .padding(.leading, 8)
+            .padding(.top, 0)
+            .padding(.bottom, 18)
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+            )
+            .font(.custom("OpenSans-Regular", size: isPad ? 18 : 14))
+            .accessibilityLabel(Text("Zone de texte pour vos impressions"))
+            .padding(.vertical)
+    }
+
+    private func shareItem() {
+        let text = "\(item.name) – \(String(format: "%.2f €", item.price))\nDécouvrez cette pièce sur l’app Joiefull!"
         let url = item.picture.url
         let activityVC = UIActivityViewController(activityItems: [text, url], applicationActivities: nil)
 
-        // Present the share sheet
         if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
            let rootVC = scene.windows.first?.rootViewController {
-            rootVC.present(activityVC, animated: true, completion: nil)
+
+            if let popover = activityVC.popoverPresentationController {
+                popover.sourceView = rootVC.view
+                popover.sourceRect = CGRect(x: rootVC.view.bounds.midX, y: 100, width: 0, height: 0)
+                popover.permittedArrowDirections = .up
+            }
+            rootVC.present(activityVC, animated: true)
         }
     }
 }
 
 
-
 #Preview {
     VStack {
-// MARK: - iPhone
-        ItemDetailView(item: .sample, isPad: false)
-    }
-}
-
-#Preview {
-    VStack {
-// MARK: - iPad
-        ItemDetailView(item: .sample, isPad: true)
+        ItemDetailView(item: .sample)
     }
 }
